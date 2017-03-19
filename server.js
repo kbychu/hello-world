6ce59ba9d9dc5588a2d831971f934ad4d4dc41d8
Kevin Chu <kbychu@gmail.com>
2017-03-18 22:32:36 -0400

    Created router.js

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var http = require("http");
+var url = require("url");
 
-http.createServer(function(request, response){
-    console.log("Request received.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-}).listen(8888);
+function start() {
+    function onRequest(request, response){
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello World");
+        response.end();
+    }
 
-console.log("Server has started.");
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started.");
+}
+
+exports.start = start;
